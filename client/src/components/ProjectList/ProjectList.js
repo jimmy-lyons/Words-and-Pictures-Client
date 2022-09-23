0113d86877a20ac3ec912c31001d74d0718d3c09
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-23 12:21:17 +0100

    project list fetches data from server

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,7 +1,19 @@
 import React from "react";
 import './ProjectList.css';
+import * as endpoints from '../../endpoints'
 
 function ProjectList (props) {
+  const getProjectData = async() => {
+    try {
+    const { data } = await endpoints.fetchProjects()
+    return data
+    } catch (error) {
+      console.log("Error when fetching projects: ", error)
+    }
+  }
+
+  console.log(getProjectData())
+
   return (
     <div className="Project-list-container">
       <p>Foo / Bar / Baz</p>
