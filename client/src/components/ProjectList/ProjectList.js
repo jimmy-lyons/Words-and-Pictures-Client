5b413402c1b927dbcb419fd95567449137aa2cbd
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-23 14:55:51 +0100

    project index prints names but has an infinte loop bug

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './ProjectList.css';
 import * as endpoints from '../../endpoints'
 
 function ProjectList (props) {
+
+  const [projectsData, setProjectData] = useState([])
+  let projects = []
+  
   const getProjectData = async() => {
     try {
     const { data } = await endpoints.fetchProjects()
@@ -12,20 +16,28 @@ function ProjectList (props) {
     }
   }
 
-  console.log(getProjectData())
+  // React is looping infinitely without this
+  // Even though projectsData only changes once, React is doing an infinite loop
+  let stop = false
+
+  useEffect(() => {
+    getProjectData().then((data) => {
+      setProjectData(data)
+      stop = true
+      console.log(projectsData)
+    })
+  }, [stop])
+
+  projects = projectsData.map((project) => {
+    return (
+      <p>{ project.name }</p>
+    )
+  })
 
   return (
-    <div className="Project-list-container">
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-      <p>Foo / Bar / Baz</p>
-    </div>
+    <>
+      { projects }
+    </>
   )
 }
 
