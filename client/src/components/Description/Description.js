8724e560e77d043801c1c498923c22c4a48e6771
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-25 11:34:25 +0100

    refactored project data into main app component

--- client/src/components/Description/Description.js
+++ client/src/components/Description/Description.js
@@ -2,9 +2,10 @@ import React from "react";
 import './Description.css';
 
 function Description (props) {
+
   return (
     <div className="Description-container">
-        { props.text }
+        { props.description }
     </div>
   )
 }
--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,30 +1,10 @@
 import React, { useEffect, useState } from "react";
 import './ProjectList.css';
-import * as endpoints from '../../endpoints'
 import ProjectListInstance from "./ProjectListIstance/ProjectListInstance";
 
 function ProjectList (props) {
 
-  const [projectsData, setProjectData] = useState([])
-  
-  const getProjectData = async() => {
-    try {
-    const { data } = await endpoints.fetchProjects()
-    return data
-    } catch (error) {
-      console.log("Error when fetching projects: ", error)
-    }
-  }
-
-  // Renders the project data once
-  useEffect(() => {
-    getProjectData().then((data) => {
-      setProjectData(data)
-      console.log(data)
-    })
-  }, [])
-
-  let projects = projectsData.map((project) => {
+  let projects = props.projectsData.map((project) => {
     return (
       <ProjectListInstance
         key={project.id}
@@ -37,7 +17,7 @@ function ProjectList (props) {
     <div>
       <h2>Projects</h2>
       <div className="project-list-container">
-        { projects }
+        {  projects } 
       </div>
     </div>
   )
