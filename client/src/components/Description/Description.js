595a6ea281255337a557ed222cc6d375296123f9
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-25 11:01:26 +0100

    tweaked description

--- client/src/components/Description/Description.js
+++ client/src/components/Description/Description.js
@@ -4,9 +4,7 @@ import './Description.css';
 function Description (props) {
   return (
     <div className="Description-container">
-      <p className="Description-text">
         { props.text }
-      </p>
     </div>
   )
 }
