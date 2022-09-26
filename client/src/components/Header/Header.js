c3a3be4ceb238b3cbf70bd4f5af8def426a73336
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-26 14:29:21 +0100

    more formatting

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,14 +4,17 @@ import ProjectList from "./ProjectList/ProjectList";
 
 function Header(props) {
   return (
-    <div className="header-container">
-      <div className="title-container">
-        ImageMusicText
+      <div className="header-container">
+        <div className="title-container">
+          ImageMusicText
+          <div className="subheading">
+            Music packaging and design by Tony Lyons and Stuart Jones
+          </div>
+        </div>
+        <ProjectList
+          projectsData={ props.projectsData }
+        />
       </div>
-      <ProjectList
-        projectsData={ props.projectsData }
-      />
-    </div>
   )
 }
 
